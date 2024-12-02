b162949fbd840c929100536eeb8e73099e113015
Drew Taylor <drew@chainofinsight.com>
2024-12-02 11:33:37 -0500

    Edit package name for on-chain

--- src/contract.rs
+++ src/contract.rs
@@ -14,7 +14,7 @@ use crate::execute::{
 use crate::query::{query_secret_ids, query_details};
 
 // version info for migration info
-const CONTRACT_NAME: &str = "crates.io:popmg";
+const CONTRACT_NAME: &str = "popmg";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
